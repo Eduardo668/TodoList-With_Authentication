fb3d8b7ce37c607f2152c648839f9d6bac297a4e
Eduardo668 <eduardosehn20@gmail.com>
2023-03-21 14:17:36 -0300

    Deploy

--- frontend/src/api/Api.ts
+++ frontend/src/api/Api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const Api = axios.create({
-    baseURL: "https://ec2-18-230-25-42.sa-east-1.compute.amazonaws.com",
+    baseURL: "https://apitodolist.eduardopaixao.tech",
     
 })
